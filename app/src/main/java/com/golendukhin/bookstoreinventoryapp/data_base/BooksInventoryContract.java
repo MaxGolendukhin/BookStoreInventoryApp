3418bcc57a53a9651f0c4e9f54ff611724865a06
Maxim Golendukhin <golendukhin.maxim@gmail.com>
2018-05-06 16:02:18 +1000

    data base contract added

---
+++ app/src/main/java/com/golendukhin/bookstoreinventoryapp/data_base/BooksInventoryContract.java
@@ -0,0 +1,20 @@
+package com.golendukhin.bookstoreinventoryapp.data_base;
+
+import android.provider.BaseColumns;
+
+public final class BooksInventoryContract {
+
+    BooksInventoryContract() {
+    }
+
+    public final static class BooksEntry implements BaseColumns {
+        public final static String BASE_NAME = "booksInventory";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_BOOKS_PRODUCT_NAME = "productName";
+        public static final String COLUMN_BOOKS_PRICE = "price";
+        public static final String COLUMN_BOOKS_QUANTITY = "quantity";
+        public static final String COLUMN_BOOKS_SUPPLIER_NAME = "supplierName";
+        public static final String COLUMN_BOOKS_SUPPLIER_PHONE_NUMBER = "supplierPhoneNumber";
+    }
+}
